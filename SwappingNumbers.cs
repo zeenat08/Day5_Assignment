a14113d7fd1f1ce1001e26543eae4ad9ed866a0c
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-15 23:26:02 +0530

    ZEENAT : Added swapping between 2 numbers program

---
+++ SwappingNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY5_Assignments
+{
+    class SwappingNumbers
+    {
+            public static void Swapping()
+            {
+                int num1, num2;
+                Console.WriteLine("Enter the first number: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the second number: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Before Swapping the numbers are: " + "Number1 = " + num1 + "," + " Number2 = " + num2);
+                num1 = num1 + num2;
+                num2 = num1 - num2;
+                num1 = num1 - num2;
+                Console.WriteLine("After swapping the numbers are: " + "Number1 = " + num1 + "," + " Number2 = " + num2);
+            }
+        }
+    }
+
