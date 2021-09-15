5ef1def44d769ecf806bd23a7afa082b493d17a9
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-15 23:21:18 +0530

    ZEENAT : Added even odd program

---
+++ EvenOdd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY5_Assignments
+{
+    class EvenOdd
+    {
+
+        public static void NumCheck()
+        {
+            int num;
+            Console.WriteLine("Enter a number to check");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else
+            {
+                Console.WriteLine("Number is odd");
+            }
+        }
+    }
+}
+    
+
