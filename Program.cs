ef96c13fc9dc2ae5d81f8632643e53332e1f1b84
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-15 23:30:00 +0530

    ZEENAT : Added compilation of main program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DAY5_Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuotientRemainder.Solution();
+            SwappingNumbers.Swapping();
+            EvenOdd.NumCheck();
+            VowelConsonants.CheckChar();
+            LargestNumber.FindLargest();
+            LeapYear.DetermineLeapYear();
+            FlipFlop.HeadTail();
+            HarmonicNumber.HarmonicNum();
+        }
+    }
+}
+           
