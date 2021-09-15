5e30c84a63eeef02f6703a8a495240b93a09fb6b
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-15 23:26:56 +0530

    ZEENAT : Added Flip flop coin program

---
+++ FlipFlop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY5_Assignments
+{
+    class FlipFlop
+    {
+        public static void HeadTail()
+        {
+            int count = 100;
+            double heads = 0;
+            double tails = 0;
+
+            for (int i = 1; i <= count; i++)
+            {
+                Random random = new Random();
+                var n = random.NextDouble();
+                if (n < 0.5)
+                {
+                    Console.WriteLine("Tail");
+                    tails += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Head");
+                    heads += 1;
+                }
+            }
+            Console.WriteLine("The percentage of Heads: " + (heads / count) * 100 + "%");
+            Console.WriteLine("The percentage of Tails: " + (tails / count) * 100 + "%");
+        }
+    }
+}
+
