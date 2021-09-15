8406b1d2275356c61608b852844fa7a202983abe
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-15 23:23:10 +0530

    ZEENAT : Added harmonic number program

---
+++ HarmonicNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY5_Assignments
+{
+    class HarmonicNumber
+    {
+        public static void HarmonicNum()
+        {
+            int num;
+            double s = 0.0;
+            Console.WriteLine("Please enter the nth number for print the harmonic series");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num == 0)
+            {
+                Console.WriteLine("U entered Zero(0)! Please enter a valid number");
+            }
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+        }
+    }
+}
