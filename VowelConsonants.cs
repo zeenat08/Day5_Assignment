7340cad08451c8fdc65f88d180cbc5005bbb2c91
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-15 23:25:23 +0530

    ZEENAT : Added vowels consonants program

---
+++ VowelConsonants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY5_Assignments
+{
+    class VowelConsonants
+    {
+        public static void CheckChar()
+        {
+            char ch;
+            Console.WriteLine("Enter a Alphabet to check Vowel or Consonant");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U')
+            {
+                Console.WriteLine("Entered alphabet " + ch + " is Vowel");
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine("Entered alphabet " + ch + " is Consonant");
+            }
+            else
+            {
+                Console.WriteLine("U entered wrong, Please enter a alphabet");
+            }
+        }
+    }
+}
+
